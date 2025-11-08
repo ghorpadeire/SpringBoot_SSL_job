79e6bb92351a714119c77c4a746ccf9e9cb8a503
Pranav Ghorpade <pranav.ghorpade3108@gmail.com>
2025-11-08 15:33:08 +0000

    Initial ssl-checker project

---
+++ src/test/java/com/pranav/ssl_checker/SslCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranav.ssl_checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SslCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
